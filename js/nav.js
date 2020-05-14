ee19ad6d7fa424c94646222841d6942bb3f4db2d
rcole1919 <59145709+rcole1919@users.noreply.github.com>
2020-05-14 16:44:50 +0400

    Загружаем файлы

---
+++ js/nav.js
@@ -0,0 +1,13 @@
+var close = document.querySelector(".page-header__toggle");
+
+var popup = document.querySelector(".site-list");
+
+popup.classList.add("site-list--close");
+close.classList.add("page-header__toggle--show");
+
+close.addEventListener("click", function (evt) {
+  "use strict";
+  evt.preventDefault();
+  popup.classList.toggle("site-list--close");
+  close.classList.toggle("page-header__toggle--open");
+});
